93bff801b8fa1c462f06a218b62e1f59d286d28d
Jena Woodburn <jena.woodburn@gmail.com>
2025-10-18 16:22:32 +0200

    Number and sort list of files output

--- sftp_migration_check.py
+++ sftp_migration_check.py
@@ -12,5 +12,5 @@ search_path = Path(check_location)
 
 found_files = [f for f in search_path.rglob("*") if f.is_file()]
 
-for f in found_files:
-    print(f)
+for i, f in enumerate(sorted(found_files), start=1):
+    print(f"{i}. {f}")
