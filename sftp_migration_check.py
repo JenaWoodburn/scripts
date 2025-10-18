f11c7cc14a5694cb50db7551ea96977e02cd9a21
Jena Woodburn <jena.woodburn@gmail.com>
2025-10-18 19:25:03 +0200

    Add ability to exclude mulitple folders from check

--- sftp_migration_check.py
+++ sftp_migration_check.py
@@ -3,7 +3,10 @@
 # Update check_location to root of the directories you want to check
 # Run this script in the directory above check_location
 # Any files found will be output to the terminal
-# 'outgoing' directories are ignored as they are where we deliver client reports. Set ignore_folder = None to include all folders
+
+# Add any folders to be ignored to the list 'ignore_folders'
+# 'outgoing' directories are ignored as they are where we deliver client reports 
+# Set ignore_folder = [] to include all folders
 
 from pathlib import Path
 
@@ -12,12 +15,12 @@ check_location = "SFTP"
 search_path = Path(check_location)
 
 # folders to ignore
-ignore_folder = "outgoing"
+ignore_folders = ["outgoing"]
 
 found_files = [
     f for f in search_path.rglob("*") 
     if f.is_file()
-    and ignore_folder not in f.parts
+    and not any(part in ignore_folders for part in f.parts)
     ]
  
 # output list of found files in numbered & sorted order
