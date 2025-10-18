b53544ba1e3ef6a7fc85a60362fca245bb35a2fb
Jena Woodburn <jena.woodburn@gmail.com>
2025-10-18 16:16:48 +0200

    Add initial SFTP Migration check file

---
+++ sftp_migration_check.py
@@ -0,0 +1,16 @@
+# Check SFTP server for files and list any found
+
+# Update check_location to root of the directories you want to check
+# Run this script in the directory above check_location
+# Any files found will be output to the terminal
+
+from pathlib import Path
+
+# root of folder structure to check
+check_location = "SFTP"
+search_path = Path(check_location)
+
+found_files = [f for f in search_path.rglob("*") if f.is_file()]
+
+for f in found_files:
+    print(f)
